63f9dccc523b0a25b55cad8a314fc1a3c461538f
juanignaciocor <juanignaciocor92@gmail.com>
2023-12-02 08:15:49 -0300

    feat: add error notification

--- src/hooks/useCreateUser.js
+++ src/hooks/useCreateUser.js
@@ -1,7 +1,10 @@
 import { rest } from "../config/axios";
 import { NAVEGATION_FEED } from "../constants/navegation";
+import { useSnackbar } from "react-simple-snackbar";
 
 const useCreateUser = () => {
+  const [openSnackbar] = useSnackbar();
+
   const handlerCreateUser = async (body) => {
     try {
       const response = await rest.post("/register", {
@@ -12,8 +15,7 @@ const useCreateUser = () => {
       localStorage.setItem("user", response.data.access_token);
       window.location.replace(NAVEGATION_FEED);
     } catch (err) {
-      // TODO : Podriamos devolver una error que nos muestre un snackbar y asi informarle al usuario
-      console.log(err);
+      openSnackbar(`Ups, ubo un error prueba mas tarde`);
     }
   };
 
